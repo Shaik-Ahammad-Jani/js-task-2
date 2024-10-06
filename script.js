8692728e3622e6e7f531718bd7dda75f01e20686
Shaik Ahammad Jani <shaikjani4403@gmail.com>
2024-10-06 20:13:32 +0530

    js task2

---
+++ script.js
@@ -0,0 +1,63 @@
+// /*----------------add,sub,mul and division ----------------using let variable--------*/
+// //addision using (let)
+// let a = 10;
+// let a1 = 20;
+// console.log("the addision of two numbers is",a+a1);
+// //subtraction using (let)
+// let b = 10;
+// let b1 = 20; 
+// console.log( "the subtraction of two numbers is", b1-b);
+// //multiplication using (let)
+// let c=10;
+// let c1=20;
+// console.log("the multiplication of two numbers is",c*c1)
+// //division using(let)
+// let d=10;
+// let d1=20;
+// console.log("the division of two numbers is",d1/d)
+
+// /*----------------add,sub,mul and division ----------------using var variable--------*/
+// // addision using (var)
+// var a = 10;
+// var a1 = 20;
+// console.log("addision of two numbers using var is",a+a1);
+// //subtraction using (var)
+// var b = 10;
+// var b1 = 20; 
+// console.log("subtraction of two numbers using var is", b1-b);
+// //multiplication using (var)
+// var c=10;
+// var c1=20;
+// console.log("multiplication of two numbers using var is",c*c1)
+// //division using(var)
+// var d=10;
+// var d1=20;
+// console.log("division of two numbers using var is",d1/d)
+
+/*----------------add,sub,mul and division ----------------using const variable--------*/
+// addision using (const)
+// const a = 10;
+// const a1 = 20;
+// console.log("addision of two numbers using const is",a+a1);
+// //subtraction using (const)
+// const b = 10;
+// const b1 = 20; 
+// console.log("subtraction of two numbers using const is", b1-b);
+// //multiplication using (const)
+// const c=10;
+// const c1=20;
+// console.log("multiplication of two numbers using const is",c*c1);
+// //division using(const)
+// const d=10;
+// const d1=20;
+// console.log("division of two numbers using const is",d1/d);
+
+
+// //not defined
+// a;
+// console.log(a)//a is not defined
+
+//undefined
+let a;
+console.log("a is ",a);//undefined
+
